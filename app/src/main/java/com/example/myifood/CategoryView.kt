6f49559c11e28befde66f7832bf13ad03eb06dcb
samfreitasxs <samuel.sfdf@gmail.com>
2023-01-02 17:09:00 -0300

    ajuste do gradle

--- app/src/main/java/com/example/myifood/CategoryView.kt
+++ app/src/main/java/com/example/myifood/CategoryView.kt
@@ -1,5 +1,16 @@
 package com.example.myifood
 
-class CategoryView: AtViewHolder{
+import android.view.ViewGroup
+import co.tiagoaguiar.atway.ui.adapter.ATViewHolder
+import com.example.myifood.databinding.CategoryItemBinding
 
-}
+class CategoryView(viewGroup: ViewGroup): ATViewHolder <Category, CategoryItemBinding> (
+    CategoryItemBinding::inflate,
+    viewGroup
+)   {
+
+    override fun bind(item: Category) {
+        binding.txtCategory.text = item.nome
+        }
+
+    }
--- app/src/main/java/com/example/myifood/RestaurantFrangment.kt
+++ app/src/main/java/com/example/myifood/RestaurantFrangment.kt
@@ -3,11 +3,17 @@ package com.example.myifood
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import co.tiagoaguiar.atway.ui.adapter.ATAdapter
 import com.example.myifood.databinding.FragmentRestaurantBinding
 
 class RestaurantFrangment : Fragment(R.layout.fragment_restaurant){
 
     private var binding: FragmentRestaurantBinding? = null
+
+    private val categoryAdapter = ATAdapter ({
+        CategoryView(it)
+    })
 
      private var filters = arrayOf(
          FilterItem(1,"Ordenar", closeIcon = R.drawable.ic_baseline_keyboard_arrow_down_24),
@@ -23,9 +29,16 @@ class RestaurantFrangment : Fragment(R.layout.fragment_restaurant){
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        categoryAdapter.items = arrayListOf(
+            Category(1, "https:ifood.com.br/static/images/categories/market.png", "Mercado", 0xFFB6D048)
+        )
+
         binding = FragmentRestaurantBinding.bind(view)
 
         binding?.let {
+            it.rvCategory.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+            it.rvCategory.adapter = categoryAdapter
+
             filters.forEach { filter ->
                 it.chipGroupFilter.addView(filter.toChip(requireContext()))
             }
